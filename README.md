e8cce550456e95d5bdf191fbe7608b1e01d2c575
diego-zunort <diegozunort@gmail.com>
2024-10-20 03:44:21 +0000

    changes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hackathon2024
 utrgv hackathon 2024
+Participants:
+
+Diego Zuniga
+Biuld Gonzalez-Lopez
+Vince Kalinowski
+Sebastian Leal Trejo
--- main.js
+++ main.js
@@ -1,12 +1,2 @@
 
-// querry selectors for forms so that 
-document.addEventListener("DOMContentloaded", () =>{
-    const businessNameForm = document.querySelector("#nameofb");
-    const subtitleForBusiness = document.querySelector("#subt");
-    const smallDescription = document.querySelector("#description");
-    const hoursOfOperation = document.querySelector("#operation");
-    const location = document.querySelector("#location");
-    const contactPhone = document.querySelector("#phone");
-    const contactEmail= document.querySelector("#email");
-});
 
